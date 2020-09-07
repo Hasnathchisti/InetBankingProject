dce125b7a0e1673a67647b30db7aadf699d29dcc
Hasnathchisti <70676210+Hasnathchisti@users.noreply.github.com>
2020-09-07 16:08:47 -0400

    Third Commit

--- src/test/java/com/inetbanking/pageObjects/AddCustomerPage.java
+++ src/test/java/com/inetbanking/pageObjects/AddCustomerPage.java
@@ -12,7 +12,7 @@ public class AddCustomerPage {
 	WebDriver ldriver;
 	
 	public AddCustomerPage(WebDriver rdriver) {
-		ldriver=rdriver;//intializing driver
+		ldriver=rdriver;//Initializing driver
 		PageFactory.initElements(rdriver, this);
 	}
 	
